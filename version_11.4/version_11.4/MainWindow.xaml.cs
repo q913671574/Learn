db78d7a01fb35c1ece414059cc82af85f00c802e
q913671574 <913671574@qq.com>
2020-11-04 23:26:14 +0800

    Add files via upload

---
+++ version_11.4/version_11.4/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace version_11._4
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool instance_f6 = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void F6_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (instance_f6)
+            {
+                F6 f6 = new F6();
+                f6.Owner = this;
+                f6.Show();
+                instance_f6 = false;
+            }
+        }
+    }
+}
